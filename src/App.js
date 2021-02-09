68b1040d23bd3cbf02aa848e4b81a44ed11639bd
romapyshka <romasadova@gmail.com>
2021-02-10 01:04:18 +0200

    Catch the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const api = {
+    key: "c14d884c9ad0731105aa7fcf29448963",
+    base: "https://api.openweathermap.org/data/2.5/"
+};
+
+// navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       console.log('latitude is: ', position.coords.latitude);
+//       console.log('longitude is: ', position.coords.longitude);
+//     }
+// );
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [city, setCity] = useState('');
+    const [weather, setWeather] = useState({});
+
+
+    // const Data = fetch(`${api.base}weather?q=${city}&appid=${api.key}`)
+    //     .then(res => res.json())
+    //     .then(result => setWeather(result));
+
+
+    // const getWeather = async () => {
+    //     const api_call = await Promise.all( () => {fetch(`${api.base}weather?q=Uzhgorod&appid=${api.key}`)});
+    //     const data = await api_call.json();
+    // };
+
+
+
+    useEffect(() => {
+      async function getData(city) {
+        return new Promise(() => {
+          fetch(`${api.base}weather?q=${city}&appid=${api.key}`)
+              .then(res => res.json())
+              .then(data => {
+                setWeather(data);
+              })
+        });
+      }
+      getData('Uzhgorod');
+
+    }, []);
+
+    console.log(weather);
+
+    return (
+        <div className="App">
+            <h2> Welcome user!</h2>
+        </div>
+    );
 }
 
 export default App;
