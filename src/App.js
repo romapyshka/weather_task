8af491e8377e961d4e394be1239968a0fe123879
romapyshka <romasadova@gmail.com>
2021-02-10 04:59:12 +0200

    Get curent location city weather data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const api = {
@@ -7,23 +7,53 @@ const api = {
 };
 
 function App() {
-    const [citySearch, setCitySearch] = useState('');
+    const [city, setCity] = useState('');
     const [weather, setWeather] = useState({});
+    const [location, setLocation] = useState('');
+    const [catchLocation, setCatchLocation] = useState(false);
+    const [catchLocalWeather, setCatchLocalWeather] = useState(false);
 
-    const search = evt => {
-            if (evt.key === "Enter") {
-                fetch(`${api.base}weather?q=${citySearch}&appid=${api.key}`)
-                    .then(res => res.json())
-                    .then(result => {
-                            setCitySearch('');
-                            setWeather(result);
-                        }
-                    );
-            }
+    useEffect(() => {
+        const getLocation = async () => {
+            return fetch('https://ipapi.co/json/', {method: "GET"})
+                .then(res => res.json())
+                .then(result => {
+                    setLocation(result.city);
+                    setCatchLocation(true);
+                })
+        };
+        if (catchLocation === false) {
+            getLocation();
         }
-    ;
-
+    }, []);
+    
+    async function getLocalWeather() {
+        if (catchLocation === true && catchLocalWeather === false) {
+            fetch(`${api.base}weather?q=${location}&appid=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                        setWeather(result);
+                        setCatchLocalWeather(true);
+                    }
+                );
+        }
+    }
+    
+    setTimeout(getLocalWeather, 1);
+    console.log(location);
     console.log(weather);
+    
+    const searchWeather = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${city}&appid=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                        setCity('');
+                        setWeather(result);
+                    }
+                );
+        }
+    };
 
     return (
         <div className="App">
@@ -33,9 +63,9 @@ function App() {
                     type="text"
                     className="search_input"
                     placeholder="Search"
-                    onChange={e => setCitySearch(e.target.value)}
-                    value={citySearch}
-                    onKeyPress={search}
+                    onChange={e => setCity(e.target.value)}
+                    value={city}
+                    onKeyPress={searchWeather}
                 />
             </div>
         </div>
@@ -43,3 +73,77 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// useEffect(() => {
+//     async function getLocation() {
+//         return new Promise(() => {
+//             if (catchLocation === false) {
+//                 fetch('https://ipapi.co/json/')
+//                     .then(res => res.json())
+//                     .then(result => {
+//                             setLocation(result);
+//                             setCatchLocation(true);
+//                         }
+//                     );
+//             }
+//             if (catchLocalWeather === false) {
+//                 fetch(`${api.base}weather?q=${location.city}&appid=${api.key}`)
+//                     .then(res => res.json())
+//                     .then(result => {
+//                             setWeather(result);
+//                             setCatchLocalWeather(true);
+//                         }
+//                     );
+//             }
+//         });
+//     }
+//     getLocation();
+//
+// }, []);
+
+// useEffect(() => {
+//         const getLocation = () => {
+//             return fetch('https://ipapi.co/json/', {method: "GET"})
+//                 .then(res => res.json())
+//                 .then(result => {
+//                     setLocation(result);
+//                     setCatchLocation(true);
+//                 });
+//         };
+//     if (catchLocation === false) {
+//         getLocation().then(r => getLocalWeather());
+//     }
+//     }, []);
+
+// function getLocation() {
+//     if (catchLocation === false) {
+//         fetch('https://ipapi.co/json/')
+//             .then(res => res.json())
+//             .then(result => {
+//                     setLocation(result);
+//                     setCatchLocation(true);
+//                 }
+//             );
+//     }
+// }
+//
+// useEffect(() => {
+//     async function getLocation() {
+//         return () => {
+//             if (catchLocation === false) {
+//                 fetch('https://ipapi.co/json/')
+//                     .then(res => res.json())
+//                     .then(result => {
+//                             setLocation(result.city);
+//                             setCatchLocation(true);
+//                         }
+//                     );
+//             }
+//         };
+//     }
+//     getLocation().then(r => setTimeout(getLocalWeather, 100));
+//
+// }, []);
